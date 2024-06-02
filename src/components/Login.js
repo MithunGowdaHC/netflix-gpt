150a639498d7ad4e151899b797ba12049577a208
MithunGowdaHC <mithungowdahc12a@gmail.com>
2024-06-02 08:20:26 +0530

    firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,23 @@
 import React from "react";
 import Header from "./Header";
 import { useState } from "react";
+import { useRef } from "react";
+import { checkValidData } from "../utils/Validate";
 
 const Login = () => {
   const [isSignInform, setIsSignInForm] = useState(true);
+
+  const [errorMessage,seterrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+  
+
+    const message = checkValidData(email.current.value, password.current.value);
+    seterrorMessage(message);
+  };
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInform);
@@ -12,9 +26,14 @@ const Login = () => {
     <div>
       <Header />
       <div className=" absolute">
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"></img>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="no"></img>
       </div>
-      <form className=" absolute w-3/12 my-32  mx-auto right-0 left-0 p-12  bg-black bg-opacity-85 rounded-lg rounded-md text-white">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className=" absolute w-3/12 my-32  mx-auto right-0 left-0 p-12  bg-black bg-opacity-85 rounded-lg rounded-md text-white"
+      >
         <h1 className=" font-bold text-3xl py-5">
           {isSignInform ? " Sign In" : "Sign Up"}
         </h1>
@@ -27,16 +46,22 @@ const Login = () => {
           ></input>
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email address"
           className=" p-4 my-3  bg-white bg-opacity-15 rounded-md w-full"
         ></input>
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className=" p-4 bg-white bg-opacity-15 my-3 rounded-md w-full"
         ></input>
-        <button className=" p-2 my-2 bg-red-700 w-full rounded-md text-center font-bold ">
+        <p className=" text-red-700 font-bold" >{errorMessage}</p>
+        <button
+          onClick={handleButtonClick}
+          className=" p-2 my-2 bg-red-700 w-full rounded-md text-center font-bold "
+        >
           {isSignInform ? " Sign In" : "Sign Up"}
         </button>
         <h3 className=" text-center p-2">OR</h3>
@@ -44,7 +69,10 @@ const Login = () => {
           Use a sign-in code
         </button>
         <button className=" w-full  text-white p-4">Forgot password?</button>
-        <p className=" py-2 font-bold" onClick={toggleSignInForm}>
+        <p
+          className=" py-2 font-bold cursor-pointer"
+          onClick={toggleSignInForm}
+        >
           {isSignInform
             ? "New to Netflix?Sign up now."
             : "Already Registered?Sign In"}
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBAQ0wgRwUxCSyXyMwqfsZ7JHtWdYU5pwM",
+  authDomain: "netflix-gpt-d3e16.firebaseapp.com",
+  projectId: "netflix-gpt-d3e16",
+  storageBucket: "netflix-gpt-d3e16.appspot.com",
+  messagingSenderId: "376149382564",
+  appId: "1:376149382564:web:66c312177ff2f860739d31",
+  measurementId: "G-HH5PZNZNGW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
